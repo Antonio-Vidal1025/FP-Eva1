079af8b98713b590bb819f936230531a18afe316
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-08-27 16:50:20 -0600

    Add files via upload

---
+++ EVA_1_HOLA_MUNDO/src/eva_1_hola_mundo/EVA_1_HOLA_MUNDO.java
@@ -0,0 +1,21 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_hola_mundo;
+
+/**
+ *
+ * @author igrid
+ */
+public class EVA_1_HOLA_MUNDO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO!!");
+        System.out.println("FUNDAMENTOS DE PROGRAMACIÓN");
+    }
+    
+}
