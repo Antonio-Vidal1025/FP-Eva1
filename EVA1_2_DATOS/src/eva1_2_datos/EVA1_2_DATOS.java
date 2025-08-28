d57167dcf52a16cd438712cce405918dab6a8a59
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-08-28 01:18:01 -0600

    Add files via upload

---
+++ EVA1_2_DATOS/src/eva1_2_datos/EVA1_2_DATOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_2_datos;
+
+/**
+ *
+ * @author igrid
+ */
+public class EVA1_2_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //; SIGNIFICA FIN DE LINEA
+        System.out.println("JOSÉ ANTONIO");
+        //NUMEROS ENTEROS
+        System.out.println(2000);
+        //NUMEROS DECIMALES
+        System.out.println(500.3);
+        //VALORES LÓGICOS (VERDADERO O FALSO)
+        //true false
+        System.out.println(true);
+        //CARACTERES INDIVIDUALES
+        //COSAS DISTINTAS
+        System.out.println('c');//CARACTER
+        System.out.println("c");//CADENA DE TEXTO
+        
+    }
+    
+}
