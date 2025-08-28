f7bc06bbbfa69585e36dc96faa7f817b786b15a9
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-08-28 01:22:12 -0600

    Add files via upload

---
+++ EVA1_4_EJERCICIO/src/eva1_4_ejercicio/EVA1_4_EJERCICIO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_ejercicio;
+
+/**
+ *
+ * @author igrid
+ */
+public class EVA1_4_EJERCICIO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Empleo: ");
+        System.out.println("Desarrollador de Microsoft");
+        System.out.print("Salario: ");
+        System.out.println("500,000 dólares al año");
+        System.out.print("Horas semanales: ");
+        System.out.println("40 horas");
+        System.out.print("Requiere Ingles?: ");
+        System.out.println(true);
+
+
+    }
+    
+}
