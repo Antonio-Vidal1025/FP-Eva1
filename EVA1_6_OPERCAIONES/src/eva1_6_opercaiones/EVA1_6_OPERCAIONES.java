c9cf0a201069d09255c607cb0c06ae99cb78303a
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-09-09 22:52:22 -0600

    Add files via upload

---
+++ EVA1_6_OPERCAIONES/src/eva1_6_opercaiones/EVA1_6_OPERCAIONES.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_6_opercaiones;
+
+/**
+ *
+ * @author igrid
+ */
+public class EVA1_6_OPERCAIONES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double pi, radio, area;
+        // ES LO MISMO QUE:
+        //double pi;
+        //double radio;
+        //double area;
+        //inicialización:
+        pi = 3.1416;
+        radio = 10;
+        //OPERACIÓN
+        area = pi * radio;
+        //SALIDA DE DATOS:
+        System.out.println("El area de un circulo de radio 10 es: ");
+        System.out.println(area);
+    }
+    
+}
