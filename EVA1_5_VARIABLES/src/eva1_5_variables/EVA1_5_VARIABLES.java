6ccf96ae349811359b6eecb691dcb87f82ea1927
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-09-09 22:48:13 -0600

    Add files via upload

---
+++ EVA1_5_VARIABLES/src/eva1_5_variables/EVA1_5_VARIABLES.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_5_variables;
+
+/**
+ *
+ * @author igrid
+ */
+public class EVA1_5_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //DECLARAR LAS VARIABLES
+        //TIPO DE DATO  NOMBRE DEL INDENTIFICARDOR
+        String nombre;
+        int edad;
+        double salario;
+        //INICIAIZACIÓN:
+        //indentificador  = valor;
+        //= --> operador de asignación
+        nombre = "José Antonio Vidal Cano";
+        edad = 17;
+        salario = 500000;
+        //edad = "Antonio"
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(salario);
+        
+        
+    }
+    
+}
