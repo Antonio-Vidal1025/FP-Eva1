cc464d417662c86dae5fe27b325f1465def21020
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-09-09 22:58:23 -0600

    Add files via upload

---
+++ EVA1_10_CAPTURA/src/eva1_10_captura/EVA1_10_CAPTURA.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_10_captura;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Antonio
+ */
+public class EVA1_10_CAPTURA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    /*BLOQUE DE COMENTARIO:
+        CALCULO DE VELOCIDAD con la formula de 
+        Velocidad = DISTANCIA / TIEMPO
+        Datos y resultados en estas unidades: m / s
+    DECLARACIÓN:  */
+    
+        double velocidad, distancia, tiempo;
+        Scanner input = new Scanner(System.in);//DECLARAR SCANNER
+        System.out.println("Introduce la distancia: ");
+        distancia = input.nexdouble
+        
+    }
