2b332212094abd6baa682247a7299c03bdf827e1
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-09-09 22:54:33 -0600

    Add files via upload

---
+++ EVA1_7_AREA_TRIANGULO/src/eva1_7_area_triangulo/EVA1_7_AREA_TRIANGULO.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_7_area_triangulo;
+
+/**
+ *
+ * @author igrid
+ */
+public class EVA1_7_AREA_TRIANGULO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double base, altura, area;
+        base = 10;
+        altura = 15;
+        area = (base * altura) / 2;
+        System.out.println("El área de un Trinagulo de base 10 y altura 15 es: ");
+        System.out.println(area);
+    }
+    
+}
