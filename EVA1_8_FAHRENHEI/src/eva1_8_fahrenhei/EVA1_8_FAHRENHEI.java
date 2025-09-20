c6781272bf3433ef708958f0759455f8bb4ca6f2
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-09-19 23:37:55 -0600

    Add files via upload

---
+++ EVA1_8_FAHRENHEI/src/eva1_8_fahrenhei/EVA1_8_FAHRENHEI.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_8_fahrenhei;
+
+/**
+ *
+ * @author Antonio
+ */
+public class EVA1_8_FAHRENHEI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    double fahrenhei, celsius, total;
+    fahrenhei = 100;
+    celsius = (fahrenhei - 32) * 5 / 9 ;
+    total = celsius;
+    System.out.println("Los grados Fahrenhei son 100");
+    System.out.println("Los grados en celsius son: ");
+    System.out.println(total );
+
+    }
+}
