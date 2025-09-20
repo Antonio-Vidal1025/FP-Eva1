ffff9cb4cb75390e0de8bd7b75930458bf7ee2b5
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-09-19 23:34:46 -0600

    Add files via upload

--- EVA1_19_ANTRO/src/eva1_19_antro/EVA1_19_ANTRO.java
+++ EVA1_19_ANTRO/src/eva1_19_antro/EVA1_19_ANTRO.java
@@ -13,24 +13,20 @@ import java.util.Scanner;
 public class EVA1_19_ANTRO {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println("****BIENVENIDO AL ANTRO XALAPAEÑO****");
-        System.out.println("Verificación de edad.");
-
-        System.out.print("¿Tiene INE? (Si/No): ");
-        String respuestaINE = input.nextLine();
-
-        if (respuestaINE.equalsIgnoreCase("Si")) {
-            System.out.print("Edad: ");
-            int edad = input.nextInt();
-
-            if (edad >= 18) {
-                System.out.println("ACCESO CONCEDIDO");
-            } else {
-                System.out.println("ACCESO DENEGADO (Eres menor de edad)");
-            }
-        } else {
-            System.out.println("ACCESO DENEGADO (No tienes INE)");
-        }
+    Scanner captu = new Scanner (System.in);
+    int edad;
+    boolean INE;
+    System.out.println("Cual es tu edad: ");
+    edad = captu.nextInt();
+    System.out.println("Tienes INE? (True/False)");
+    INE = captu.nextBoolean();
+    //USAMOS AND &&
+    //mayor de edad y tener INE
+    //if((edad >= 18) && (INE == true))
+    if((edad >= 18) && (INE == true)){
+        System.out.println("Puedes  entrar");
+    }else{
+        System.out.println("No puedes entrar");
+    }
     }
 }
