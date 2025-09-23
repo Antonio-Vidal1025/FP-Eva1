7e6ffa2213d01a1a2b70114f52d3299510762f65
Antonio-Vidal1025 <L25550647@chihuahua2.tecnm.mx>
2025-09-22 23:13:37 -0600

    Add files via upload

---
+++ EVA1_24_JUEGO_AGUILA_SOL/src/eva1_24_juego_aguila_sol/EVA1_24_JUEGO_AGUILA_SOL.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_24_juego_aguila_sol;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Antonio
+ */
+public class EVA1_24_JUEGO_AGUILA_SOL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner captu = new Scanner (System.in);
+       int usuario; 
+       //System.out.println("Aguilao o sello? (1/0): ");
+       //usuario = Captu.nextInt();
+       System.out.println(Math.random());
+       //IMPRIMIR QUE CAE LA MONEDA (AGUILA O SELLO) SEGUN EL VALOR RANDOM
+    }
+    
+}
